da64ef5e4fc61c37d6cf65da799e106a3480c23f
Alapont <pablodragon@gmail.com>
2018-07-23 17:20:31 +0200

    initial writting for csv writter

--- Solar/Workers/csv-maker.py
+++ Solar/Workers/csv-maker.py
@@ -1,13 +1,25 @@
-class CsvMaker(object):
+from Workers import worker
+class CsvMaker(worker):
     #gets a lot of workers and serializes them to work united as one
     def start(self,fileName=None,headder="TIMESTAMP,ARFISOL,BSRN,CESA,DISS,KONTAS,PSA,TSA-1,TSA-2,"):
         if fileName == None:
-            self=None
+            self=None #seppuku
         else:
             self._fileName=fileName;
     def input(self,data):
-        for worker in self.workers:
-            worker.input(data);
+        with open(self._fileName, 'a') as fd:
+          fd.write(_buildLine(data))
     def results(self):
-        for worker in self.workers:
-            worker.results();
+        #if one file is opened for the whole writting duration, close it.
+        #multiple oppenings are concerning
+        pass
+
+    def _buildLine(atributes,timestamp='TIMESTAMP',separator=','):
+        if (timestamp=='TIMESTAMP'):
+            return headder
+        line='\n'+timestamp
+        for elem in atributes:
+            line+=','+_ftos(elem)
+        return line
+    def _ftos(num):#needed to maintain style with libreOffice. who am I to argue
+        return ('"'+str(num).replace('.',',')+'"')
